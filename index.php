c16ee862d3d04a1a5837cfe7a31ba406e749db9a
ahmetcankal <acankal@gmail.com>
2015-11-02 16:45:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    // put your code here
+    ?>
+    
+    <form action="yekle.php" method="POST">
+        yemek adı:<input type="text" name="yemekadi">
+            <input type="submit" value="yemek ekle">
+                
+        
+    </form>
+</body>
+</html>
